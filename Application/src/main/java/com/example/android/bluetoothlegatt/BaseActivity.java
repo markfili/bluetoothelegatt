829e47b0e5506e5e249cf2f320b837b0edb08c92
Marko Filipovic <mrkfilipovic3@gmail.com>
2016-02-18 16:06:10 +0100

    debug to false

--- Application/src/main/java/com/example/android/bluetoothlegatt/BaseActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/BaseActivity.java
@@ -10,10 +10,8 @@ import android.widget.Toast;
 public class BaseActivity extends Activity {
 
     // set to true to enable logs, test data (paired devices) and toasts
-    public static boolean DEBUG = true;
-        /*
-    DEBUG LOG & TOAST
-     */
+    public static boolean DEBUG = false;
+        /* DEBUG LOG & TOAST */
 
     public void logD(String TAG, String message) {
         if (DEBUG) Log.d(TAG, message);
